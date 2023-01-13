8e01be56b88d3412748c38ef155cfdd099eae4f0
Nico Lueg <nicolueg@yahoo.de>
2023-01-13 06:07:17 +0100

    fix(filter): filter solutionparts

--- src/app/algorithms/regions/petri-net-regions.service.ts
+++ src/app/algorithms/regions/petri-net-regions.service.ts
@@ -206,7 +206,23 @@ export class PetriNetRegionsService {
           )
           .filter((solution) => solution.length > 0),
       }))
-      .filter((solution) => solution.solutionParts.length > 0);
+      .filter((solution) => {
+        if (solution.solutionParts.length === 0) {
+          return false;
+        }
+        solution.solutionParts = solution.solutionParts.filter(
+          (value, index) => {
+            const stringifiedValue = JSON.stringify(value);
+            return (
+              index ===
+              solution.solutionParts.findIndex(
+                (obj) => JSON.stringify(obj) === stringifiedValue
+              )
+            );
+          }
+        );
+        return true;
+      });
 
     return solutionsWithMaybeDuplicates.filter((value, index) => {
       const stringifiedValue = JSON.stringify(value.solutionParts);
